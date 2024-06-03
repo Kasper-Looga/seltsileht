2abed23b54f53b8bf1554636a5427a48a4372c3c
Christo Vahtramäe <christo.vahtramae@iglu.ee>
2024-06-03 12:26:00 +0300

    Organize project

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
-import './App.css';
-import Button from './Buttontest';
-import Background from  './Images/Background.png';
-import Logo from './Images/Logo1.png';
+import "./App.css";
+import Button from "./components/Buttontest";
+import Background from "./Images/Background.png";
+import Logo from "./Images/Logo1.png";
 
-
-console.log(Logo)
-console.log(Background)
+console.log(Logo);
+console.log(Background);
 
 function App() {
-
-  const handleClick = ()  =>{
-    alert('Button Clicked')
-  }
+  const handleClick = () => {
+    alert("Button Clicked");
+  };
 
   return (
     <div className="App">
       <header className="App-header">
+        <img src={Logo} className="App-logo" alt="Logo peaks siin olema" />
 
-        <img src={Logo} className="App-logo" alt="Logo peaks siin olema"/>
-
-        <button onClick={handleClick}>
-         click me
-        </button>
+        <Button onClick={handleClick}>click me</Button>
       </header>
     </div>
   );
