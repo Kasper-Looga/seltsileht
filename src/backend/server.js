9173f0160830db13eb6c839dc298d618e7ad555e
Kasper-Looga <Kasper.looga@gmail.com>
2024-06-20 13:18:30 +0300

    trying to get login info from backend

--- src/backend/server.js
+++ src/backend/server.js
@@ -10,20 +10,35 @@ const PORT = process.env.Port || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
-const uri = "mongodb://localhost:27017/Data"
-mongoose.connect(uri,{useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() => console.log("Mongodb connected"))
-    .catch(err => console.log(err, "MongoDb not connected"))
+const uri = "mongodb://localhost:27017/Data";
+const uri2 = "mongodb://localhost:27017/Login";
+mongoose
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Mongodb connected data"))
+  .catch((err) => console.log(err, "MongoDb not connected data "));
 
+mongoose
+  .connect(uri2, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Mongodb connected Login"))
+  .catch((err) => console.log(err, "MongoDb not connected Login "));
 
-app.get("/data", async(req,res) => {
-    try{
-        const data = await Data.find()
-        console.log("Fetched Data", data)
-        res.json(data)
-    } catch(err){
-        res.status(500).json({message: err.message});
-    }
+app.get("/data", async (req, res) => {
+  try {
+    const data = await Data.find();
+    console.log("Fetched Data", data);
+    res.json(data);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+app.get("/Login", async (req, res) => {
+  try {
+    const data = await Data.find();
+    console.log("Fetched Data", data);
+    res.json(data);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
 app.listen(PORT, () => console.log("Server is running on Port 5000"));
