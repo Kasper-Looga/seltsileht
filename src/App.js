2f0752e099366905e002eedf8b08c345e18710b1
Kasper-Looga <Kasper.looga@gmail.com>
2024-06-27 11:14:48 +0300

    trying to make login form

--- src/App.js
+++ src/App.js
@@ -53,7 +53,77 @@ function App() {
           </Button>
         </div>
         <div className="ButtonContainer registerbutton">
-          <Button onClick={() => fetchData("info")}>Login </Button>
+          <Button
+            onClick={(document.getElementById("id_01").style.display = "block")}
+          >
+            Login{" "}
+          </Button>
+          <div id="id_01">
+            <form>
+              <div>
+                <h1> Sign up</h1>
+                <p>Fill out this form now</p>
+                <label for="email">
+                  <b>Email</b>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Enter Password"
+                  name="psw"
+                  required
+                ></input>
+
+                <label for="psw">
+                  <b>Password</b>
+                </label>
+                <input
+                  type="password"
+                  placeholder="Enter Password"
+                  name="psw"
+                  required
+                ></input>
+
+                <label for="psw-repeat">
+                  <b>Repeat Password</b>
+                </label>
+                <input
+                  type="password"
+                  placeholder="Repeat Password"
+                  name="psw-repeat"
+                  required
+                ></input>
+
+                <label>
+                  <input
+                    type="checkbox"
+                    checked="checked"
+                    name="remember"
+                    style="margin-bottom:15px"
+                  >
+                    {" "}
+                    Remember me
+                  </input>
+                </label>
+
+                <p>
+                  By creating an account you agree to our{" "}
+                  <a href="#" style="color:dodgerblue">
+                    Terms & Privacy
+                  </a>
+                  .
+                </p>
+
+                <div class="clearfix">
+                  <button type="button" class="cancelbtn">
+                    Cancel
+                  </button>
+                  <button type="submit" class="signupbtn">
+                    Sign Up
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
         </div>
 
         {displayItems && (
